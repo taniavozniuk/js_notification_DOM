1d960dcbaa28c7876d6d0a40d25f55908863bf27
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2024-11-20 10:13:36 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    // створюю контейнер повідомлення
+    const notification = document.createElement("div");
+    // додаю клас notification і типу (success, error, warning)
+    notification.classList.add("notification", type);
+    // встановлення координат
+    notification.style.position = "absolute";
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    // додаю заголовок
+    const titleElement = document.createElement("h3");
+    titleElement.classList.add("title");
+    titleElement.textContent = title;
+    notification.appendChild(titleElement);
+    // додаю опис
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = description;
+    notification.appendChild(descriptionElement);
+    // додаю елемент до документа
+    document.body.appendChild(notification);
+    // видалення елемента через 2 с
+    setTimeout(()=>{
+        notification.remove();
+    }, 200000000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
